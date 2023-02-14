01f02d859b98eea1fb63ca3ee32d27db6a255010
Nagy Attila <nagy.attisr.50@gmail.com>
2023-02-14 12:29:41 +0100

    Admin alap

--- Akasztofa/admin.xaml.cs
+++ Akasztofa/admin.xaml.cs
@@ -31,5 +31,109 @@ namespace Akasztofa
             this.Close();
             mw.Show();
         }
+
+        private void betoltes(object sender, RoutedEventArgs e)
+        {
+            List<string> lista = new List<string>();
+            lista.Add("Könnyű");
+            lista.Add("Közepes");
+            lista.Add("Nehéz");
+            foreach (var item in lista)
+            {
+                ca1.Items.Add(item);
+            }
+            
+        }
+
+        private void fasza(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            
+        }
+
+        private void faszom(object sender, SelectionChangedEventArgs e)
+        {
+            int nehezseg = 0;
+            if (ca1.SelectedItem == "Könnyű")
+            {
+                nehezseg = 1;
+                la1.Content = "Könnyű: 1-7 karakter";
+            }
+            if (ca1.SelectedItem == "Közepes")
+            {
+                nehezseg = 2;
+                la1.Content = "Közepes: 8-12 karakter";
+            }
+            if (ca1.SelectedItem == "Nehéz")
+            {
+                nehezseg = 3;
+                la1.Content = "Nehéz: 12-nél több";
+            }
+        }
+
+        private void feltoltes(object sender, RoutedEventArgs e)
+        {
+            if (ta1.Text.All(char.IsLetter))
+            {
+                if (ca1.SelectedIndex == -1)
+                {
+                    MessageBox.Show("Nem választott nehézségi szintet","Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                ta1.Text = string.Empty;
+                MessageBox.Show("Nem szöveget írt be", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void pirosRoman(object sender, TextChangedEventArgs e)
+        {
+            int karakterE = 0;
+            int karakterV = 0;
+            if (ca1.SelectedItem == "Könnyű")
+            {
+                karakterE = 1;
+                karakterV = 7;
+                if (ta1.Text.Length < karakterE || ta1.Text.Length > karakterV)
+                {
+                    la1.Foreground = new SolidColorBrush(Colors.Red);
+                }
+                else
+                {
+                    la1.Foreground = new SolidColorBrush(Colors.Green);
+                }
+            }
+            if (ca1.SelectedItem == "Közepes")
+            {
+                karakterE = 8;
+                karakterV = 12;
+                if (ta1.Text.Length < karakterE || ta1.Text.Length > karakterV)
+                {
+                    la1.Foreground = new SolidColorBrush(Colors.Red);
+                }
+                else
+                {
+                    la1.Foreground = new SolidColorBrush(Colors.Green);
+                }
+            }
+            if (ca1.SelectedItem == "Nehéz")
+            {
+                karakterE = 12;
+                karakterV = 99;
+                if (ta1.Text.Length < karakterE || ta1.Text.Length > karakterV)
+                {
+                    la1.Foreground = new SolidColorBrush(Colors.Red);
+                }
+                else
+                {
+                    la1.Foreground = new SolidColorBrush(Colors.Green);
+                }
+            }
+        }
+
+        private void Delete(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
