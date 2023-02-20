4f39e456dbd3438faa1c9ef3bd1b160ffb448a50
Nagy Attila <nagy.attisr.50@gmail.com>
2023-02-20 15:05:08 +0100

    balfaszvagyok

---
+++ Akasztofa/jatekter.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Drawing.Bitmap;
+using System.Drawing.Imaging.Metafile;
+
+namespace Akasztofa
+{
+    /// <summary>
+    /// Interaction logic for jatekter.xaml
+    /// </summary>
+    public partial class jatekter : Window
+    {
+        public jatekter()
+        {
+            InitializeComponent();
+        }
+
+        private void load(object sender, RoutedEventArgs e)
+        {
+            image = Image.FromFile("C:\\Users\\nagya\\source\\repos\\0Miki1\\Akasztofa\\Akasztofa\\frames");
+        }
+    }
+}
